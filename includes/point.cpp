4b8594f33dce2c4314c09bc3bc98a05abc78795e
ssinalpha <hanhdhx65@gmail.com>
2023-12-04 16:52:05 +0900

    이것이 다형성이다

--- includes/point.cpp
+++ includes/point.cpp
@@ -9,10 +9,10 @@
 #include "point.h"
 
 Point::Point()
-	: x(0.0), y(0.0)
+	: x{ 0.0 }, y{ 0.0 }
 {
 };
 
-Point::Point(double a, double b) : x(a), y(b)
+Point::Point(double a, double b) : x{ a }, y{ b }
 {
 };

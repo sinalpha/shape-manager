3d7ffc3f9d4af950ded7310e9c5c4d7b1bf37aff
ssinalpha <hanhdhx65@gmail.com>
2023-12-07 00:45:13 +0900

    메뉴 help 잘못됨을 고침

--- src/utility.h
+++ src/utility.h
@@ -30,7 +30,7 @@ enum SHAPE_TYPE {
 	LINE
 };
 
-const std::string help = "\n0: 프로그램 종료하기\n1 (도형) (생성 방법) : 원하는 도형 추가\n2 : 전체 도형을 그리기\n3 : n번째 도형을 제거\n4 : 원하는 도형을 모두 제거\n5: 저장하기\n6: 불러오기\n";
+const std::string help = "\n0: 프로그램 종료하기\n1 (도형) (생성 방법) : 원하는 도형 추가\n2 : 전체 도형을 그리기\n3 : 원하는 도형을 모두 제거\n4 : n번째 도형을 제거\n5: 저장하기\n6: 불러오기\n";
 const std::string insertHelp = "1 (도형) (생성 방법)\n도형		: 1 - 원, 2 - 직사각형, 3 - 삼각형, 4 - 선\n생성 방법	: default - 기본 생성, 2 - 값을 입력해서 생성\n도형을 만드는데 실패하였습니다";
 
 Shape* selShape(int, int);
